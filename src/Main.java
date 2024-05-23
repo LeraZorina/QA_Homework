b442d594e81e99d2eeba4cb79a332860947f9891
Lera Zorina <valeria.sidorenko95@gmail.com>
2024-05-23 14:18:32 +0300

    Lesson_4 done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Employee[] empArray = new Employee[5];
-        empArray[0] = new Employee("Ivanov Ivan", "Engineer",
-                "ivivan@mailbox.com", "892312312", 30000, 30);
-        empArray[1] = new Employee("Petrov Petr", "QA",
-                "petr@mail.ru", "89618794566", 90000, 35);
-        empArray[2] = new Employee("Sidorova Irina", "AQA",
-                "iriii@mailbox.com", "89156668899", 80000, 29);
-        empArray[3] = new Employee("Arova Polina", "Engineer",
-                "arova@mail.ru", "89564389788", 45000, 32);
-        empArray[4] = new Employee("Markov Mark", "Analyst",
-                "markmark@gmail.com", "89167544889", 120000, 33);
+        empArray[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        empArray[1] = new Employee("Petrov Petr", "QA", "petr@mail.ru", "89618794566", 90000, 35);
+        empArray[2] = new Employee("Sidorova Irina", "AQA", "iriii@mailbox.com", "89156668899", 80000, 29);
+        empArray[3] = new Employee("Arova Polina", "Engineer", "arova@mail.ru", "89564389788", 45000, 32);
+        empArray[4] = new Employee("Markov Mark", "Analyst", "markmark@gmail.com", "89167544889", 120000, 33);
         System.out.println("Задание №1\n");
         for (Employee employee : empArray)
             employee.info();

8c8f19ab131f02d3de1e453766ff3924f78b5cc4
Lera Zorina <valeria.sidorenko95@gmail.com>
2024-06-24 13:53:08 +0300

    Lesson_14_testng

--- src/test/java/TestFactorial.java
+++ src/test/java/TestFactorial.java
@@ -19,12 +19,16 @@ public class TestFactorial {
         assertEquals(BigInteger.valueOf(1), factorial.CalcFactorial(0));
     }
 
-    @Test (description = "Проверка факториала для натурального числа больше 0")
+    @Test(description = "Проверка факториала для единицы")
+    void testWithOne() {
+        assertEquals(BigInteger.valueOf(1), factorial.CalcFactorial(1));
+    }
+
+    @Test(description = "Проверка факториала для натурального числа больше 1")
     void testPositive() {
         BigInteger result = new BigInteger("20922789888000");
         assertEquals(result, factorial.CalcFactorial(16));
         assertEquals(BigInteger.valueOf(120), factorial.CalcFactorial(5));
-        assertEquals(BigInteger.valueOf(1), factorial.CalcFactorial(1));
     }
 
     @Test(description = "Проверка на ввод отрицательного значения")

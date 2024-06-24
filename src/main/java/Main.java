405aacb6c1094369681d47d47845815821203f61
Lera Zorina <valeria.sidorenko95@gmail.com>
2024-06-24 10:33:57 +0300

    Lesson_13_fixing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Задание №1\n");

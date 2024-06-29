90b43863dee4d186eb368979febf0ce905d8d9de
Lera Zorina <valeria.sidorenko95@gmail.com>
2024-06-29 13:38:11 +0300

    Lesson_16

--- src/test/java/MtsTest.java
+++ src/test/java/MtsTest.java
@@ -30,48 +30,13 @@ public class MtsTest {
         }
     }
 
-    @DisplayName("Проверить название блока онлайн пополнения")
+    @DisplayName("Проверка надписей в незаполненных полях варианта оплаты услуг: услуги связи")
     @Test
     void blockNameTest() {
         String blockName = driver.findElement(By.xpath("//*[@class='pay__wrapper']//h2")).getText();
         String expName = "Онлайн пополнение\n" +
                 "без комиссии";
         assertEquals(expName, blockName);
-    }
-
-    @DisplayName("Проверить наличие логотипов платёжных систем")
-    @Test
-    void availabilityLogoTest() {
-        List<WebElement> logoSystems = driver.findElements(By.xpath("//*[@class='pay__partners']//img[contains(@alt,'Visa')]"));
-        logoSystems.addAll(driver.findElements(By.xpath("//*[@class='pay__partners']//img[contains(@alt,'MasterCard')]")));
-        logoSystems.add(driver.findElement(By.xpath("//*[@class='pay__partners']//img[contains(@alt,'Белкарт')]")));
-        assertEquals(5, logoSystems.size());
-    }
-
-    @DisplayName("Проверить работу ссылки «Подробнее о сервисе»")
-    @Test
-    void linkTest() {
-        driver.findElement(By.xpath("//*[@class='pay__wrapper']//a[text()='Подробнее о сервисе']")).click();
-        String expLink = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
-        String actLink = driver.getCurrentUrl();
-        assertEquals(expLink, actLink);
-        driver.switchTo().defaultContent();
-    }
-
-    @DisplayName("Проверить работу кнопки «Продолжить» после заполнения полей")
-    @Test
-    void onlineReplenishmentTest() {
-        WebElement connectionPhone = driver.findElement(By.id("connection-phone"));
-        WebElement connectionSum = driver.findElement(By.id("connection-sum"));
-        connectionPhone.click();
-        connectionPhone.sendKeys("297777777");
-        connectionSum.click();
-        connectionSum.sendKeys("20");
-        driver.findElement(By.xpath("//*[@id='pay-connection']/button")).click();
-        WebElement frame = driver.findElement(By.xpath("//iframe[@class='bepaid-iframe']"));
-        String link = frame.getAttribute("src");
-        assertEquals("https://checkout.bepaid.by/widget_v2/index.html", link);
-        driver.get(mainUrl);
     }
 
     @AfterAll

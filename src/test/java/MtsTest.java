d5d1555355bb4036b41154ff1265f23526d5dc9f
Lera Zorina <valeria.sidorenko95@gmail.com>
2024-07-01 13:32:10 +0300

    Lesson_16

--- src/test/java/MtsTest.java
+++ src/test/java/MtsTest.java
@@ -2,8 +2,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -79,6 +83,68 @@ public class MtsTest {
                 () -> assertEquals("Сумма", fieldSum),
                 () -> assertEquals("E-mail для отправки чека", fieldEmail)
         );
+    }
+
+    @DisplayName("Проверка корректности отображения суммы (в том числе на кнопке), номера телефона")
+    @Test
+    void displayAmountAndPhoneTest() {
+        switchToFrame();
+        String textLabel = driver.findElement(By.xpath("//*[@class='pay-description__cost']//span[1]")).getText();
+        String textButton = driver.findElement(By.xpath("//* [@class='card-page__card']//button")).getText();
+        String textPhone = driver.findElement(By.xpath("//* [@class='pay-description__text']")).getText();
+        assertAll(
+                () -> assertEquals("20.00 BYN", textLabel),
+                () -> assertEquals("Оплатить 20.00 BYN", textButton),
+                () -> assertEquals("Оплата: Услуги связи Номер:375297777777", textPhone)
+        );
+        clearData();
+    }
+
+    @DisplayName("Проверка надписей в незаполненных полях для ввода реквизитов карты")
+    @Test
+    void placeHolderCardTest() {
+        switchToFrame();
+        String fieldCardNum = driver.findElement(By.xpath("//label[contains(@class,'ng-tns-c46-1 ng-star-inserted')]")).getText();
+        String fieldValidity = driver.findElement(By.xpath("//label[contains(@class,'ng-tns-c46-4 ng-star-inserted')]")).getText();
+        String fieldCvc = driver.findElement(By.xpath("//label[contains(@class,'ng-tns-c46-5 ng-star-inserted')]")).getText();
+        String fieldHolderName = driver.findElement(By.xpath("//label[contains(@class,'ng-tns-c46-3 ng-star-inserted')]")).getText();
+        assertAll(
+                () -> assertEquals("Номер карты", fieldCardNum),
+                () -> assertEquals("Срок действия", fieldValidity),
+                () -> assertEquals("CVC", fieldCvc),
+                () -> assertEquals("Имя держателя (как на карте)", fieldHolderName)
+        );
+        clearData();
+    }
+
+    @DisplayName("Проверка наличия иконок платёжных систем")
+    @Test
+    void paymentSystemIconsTest() {
+        switchToFrame();
+        List<WebElement> icons = driver.findElements(By.xpath("//*[@class='cards-brands ng-tns-c46-1']//img"));
+        assertEquals(5, icons.size());
+        clearData();
+    }
+
+    private void switchToFrame() {
+        WebElement connectionPhone = driver.findElement(By.id("connection-phone"));
+        WebElement connectionSum = driver.findElement(By.id("connection-sum"));
+        connectionPhone.click();
+        connectionPhone.sendKeys("297777777");
+        connectionSum.click();
+        connectionSum.sendKeys("20");
+        driver.findElement(By.xpath("//*[@id='pay-connection']/button")).click();
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@class='bepaid-iframe']"));
+        driver.switchTo().frame(iframe);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@class='pay-description__cost']//span[1]"))));
+    }
+
+    private void clearData() {
+        driver.findElement(By.xpath("//svg-icon[@class='header__close-icon']")).click();
+        driver.switchTo().defaultContent();
+        driver.findElement(By.id("connection-phone")).clear();
+        driver.findElement(By.id("connection-sum")).clear();
     }
 
     @AfterAll
